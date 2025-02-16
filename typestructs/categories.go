a90f07984787b3df7e9756caa749c32f57116f46
riziqalbab <albabriziq36@gmail.com>
2025-02-16 16:12:51 +0700

    feat(categories): expand file format support for categories

--- typestructs/categories.go
+++ typestructs/categories.go
@@ -8,7 +8,7 @@ type FileCategory struct {
 var Categories = []FileCategory{
 	{
 		Name:   "image",
-		Format: []string{"jpg", "jpeg", "png", "gif", "bmp", "svg"},
+		Format: []string{"jpg", "jpeg", "png", "gif", "bmp", "svg", "webp", "heic", "heif"},
 	},
 	{
 		Name:   "video",
@@ -20,7 +20,7 @@ var Categories = []FileCategory{
 	},
 	{
 		Name:   "document",
-		Format: []string{"doc", "docx", "pdf", "xls", "xlsx", "ppt", "pptx"},
+		Format: []string{"doc", "docx", "pdf", "xls", "xlsx", "ppt", "pptx", "csv"},
 	},
 	{
 		Name:   "archive",
@@ -28,6 +28,10 @@ var Categories = []FileCategory{
 	},
 	{
 		Name:   "programs",
-		Format: []string{"exe", "msi", "sh"},
+		Format: []string{"exe", "msi", "sh", "jar"},
+	},
+	{
+		Name:   "code",
+		Format: []string{"sql", "html", "css", "js", "php", "py", "c", "cpp", "go", "java", "sh", "rb", "swift", "ts", "json", "xml", "yml", "yaml"},
 	},
 }
