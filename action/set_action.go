c9dc99e5dc97bbda96c67006d8d45894a8113b39
riziqalbab <albabriziq36@gmail.com>
2025-02-18 17:09:12 +0700

    feat(set): add validation for file prefix, suffix, and destination folder

--- action/set_action.go
+++ action/set_action.go
@@ -2,11 +2,31 @@ package action
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/urfave/cli/v3"
 )
 
 func SetAction(ctx context.Context, cmd *cli.Command) error {
+	// pathsArg := cmd.Args().Get(0)
+	// workingDirectoryPath, err := os.Getwd()
 
+	file_prefix_name := cmd.String("file_prefix")
+	file_suffix_name := cmd.String("file_suffix")
+	folder_destination := cmd.String("to_folder")
+
+	if file_prefix_name == "" && file_suffix_name == "" {
+		fmt.Println("Please specify a file prefix or a file suffix. For example: folderizer set --file_prefix <file_prefix_name> --file_suffix <file_suffix_name> --to_folder <folder_destination>")
+		return nil
+	}
+	if folder_destination == "" {
+		fmt.Println("Please specify a folder destination. For example: folderizer set --file_prefix <file_prefix_name> --file_suffix <file_suffix_name> --to_folder <folder_destination>")
+		return nil
+	}
+	organizeSet(ctx, cmd)
 	return nil
+}
+
+func organizeSet(ctx context.Context, cmd *cli.Command) {
+
 }
